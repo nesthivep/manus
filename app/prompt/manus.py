051d06a7ba0f89f89d88ce36c35e03e13a1a0908
samanhappy <samanhappy@gmail.com>
2025-03-18 13:47:50 +0800

    add anp tool

--- app/prompt/manus.py
+++ app/prompt/manus.py
@@ -1,6 +1,6 @@
 SYSTEM_PROMPT = "You are OpenManus, an all-capable AI assistant, aimed at solving any task presented by the user. You have various tools at your disposal that you can call upon to efficiently complete complex requests. Whether it's programming, information retrieval, file processing, or web browsing, you can handle it all."
 
-NEXT_STEP_PROMPT = """You can interact with the computer using PythonExecute, save important content and information files through FileSaver, open browsers with BrowserUseTool, and retrieve information using GoogleSearch.
+NEXT_STEP_PROMPT = """You can interact with the computer using PythonExecute, save important content and information files through FileSaver, open browsers with BrowserUseTool, and retrieve information using GoogleSearch, and interact with other agents using ANPTool.
 
 PythonExecute: Execute Python code to interact with the computer system, data processing, automation tasks, etc.
 
@@ -9,6 +9,8 @@ FileSaver: Save files locally, such as txt, py, html, etc.
 BrowserUseTool: Open, browse, and use web browsers. If you open a local HTML file, you must provide the absolute path to the file.
 
 Terminate: End the current interaction when the task is complete or when you need additional information from the user. Use this tool to signal that you've finished addressing the user's request or need clarification before proceeding further.
+
+ANPTool: Interact with other agents using Agent Network Protocol (ANP).ANP provides services such as hotel and scenic spot ticket query and booking.
 
 Based on user needs, proactively select the most appropriate tool or combination of tools. For complex tasks, you can break down the problem and use different tools step by step to solve it. After using each tool, clearly explain the execution results and suggest the next steps.
 
